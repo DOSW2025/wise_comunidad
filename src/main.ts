ac58de6e500a9f48ca1cc7f27ec8f4c8b271b418
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2025-11-11 18:46:58 -0500

    add env port variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
+
+  const logger = new Logger('wise_comunidad');
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+
+  logger.log(`wise_comunidad esta corriendo en el puerto: ${envs.port}`);
 }
 bootstrap();
