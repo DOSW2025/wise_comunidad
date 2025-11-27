e84ca3ba15a654d9081d1de5b0c8577a9b43ffc9
DannielEU <du.important@gmail.com>
2025-11-27 11:02:07 -0500

    Enhance main.ts by adding CORS support and global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
 
 async function bootstrap() {
   const logger = new Logger('ComunidadApp');
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  
   await app.listen(envs.port);
   logger.log(`Application is running on: ${envs.port}`);
 }
