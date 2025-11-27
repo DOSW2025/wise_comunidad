66aae942a30a4f26e27826231f06b89e87140ae5
DannielEU <du.important@gmail.com>
2025-11-27 11:00:20 -0500

    Refactor main.ts to use environment configuration for port and add logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
+  const logger = new Logger('ComunidadApp');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.port);
+  logger.log(`Application is running on: ${envs.port}`);
 }
 bootstrap();
