65025ef72ebb3bd17900356e6139f8efff518fb6
LauraVenegas6 <aleja09venegas@gmail.com>
2025-12-02 16:26:15 -0500

    feat(configuracion): se configuro el swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config/envs';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('ComunidadApp');
@@ -18,7 +19,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(envs.port);
+
+  // 🔥 Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Wise Comunidad API')
+    .setDescription('API documentation for the Wise Comunidad microservice')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(envs.port, '0.0.0.0');
   logger.log(`Application is running on: ${envs.port}`);
 }
-void bootstrap();
+bootstrap();
